53c4ec4a2e6f0c8cd35d8a7b749ee7bed5f9fc43
Shinya Tsnematsu <s@tnmt.info>
2024-04-07 04:15:24 +0900

    update nixpkgs.url

--- flake.nix
+++ flake.nix
@@ -1,15 +1,13 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  };
+  inputs = { nixpkgs.url = "nixpkgs/nixos-22.11"; };
 
   outputs = inputs: {
     nixosConfigurations = {
       myNixOS = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-	modules = [
-	  ./configuration.nix
-	];
+        modules = [
+        ./configuration.nix
+        ];
       };
     };
   };
