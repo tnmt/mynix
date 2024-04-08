1c27ea61378969d3d527e8fd0a3d415aa03b57f0
Shinya Tsunematsu <s@tnmt.info>
2024-04-09 00:20:36 +0900

    retry remote build

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
         system = "x86_64-linux";
         modules = [
           ./hosts/sunflower
-          # nixos-hardware.nixosModules.microsoft-surface-go
+          nixos-hardware.nixosModules.microsoft-surface-go
         ];
       };
     };
