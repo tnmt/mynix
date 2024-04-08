e7fd1563d51ba7784b72d98ce724c68af0a437ba
Shinya Tsunematsu <s@tnmt.info>
2024-04-08 18:56:25 +0900

    setup remote build

--- hosts/sunflower/default.nix
+++ hosts/sunflower/default.nix
@@ -5,4 +5,22 @@
   ];
 
   networking.hostName = "sunflower";
+
+  nix.buildMachines = [ {
+	  hostName = "192.168.122.30";
+	  system = "x86_64-linux";
+    protocol = "ssh-ng";
+	  # if the builder supports building for multiple architectures,
+	  # replace the previous line by, e.g.
+	  # systems = ["x86_64-linux" "aarch64-linux"];
+	  maxJobs = 1;
+	  speedFactor = 2;
+	  supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+	  mandatoryFeatures = [ ];
+	}] ;
+	nix.distributedBuilds = true;
+	# optional, useful when the builder has a faster internet connection than yours
+	nix.extraOptions = ''
+	  builders-use-substitutes = true
+	'';
 }
