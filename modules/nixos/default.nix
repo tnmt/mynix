149a86309a23fb538921558c3dfd15ac7432d794
Shinya Tsunematsu <s@tnmt.info>
2024-04-08 17:28:07 +0900

    fix warnings for xkb

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -46,8 +46,10 @@
 
   # Configure keymap in X11
   services.xserver = {
-    layout = "us";
-    xkbVariant = "";
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
   };
 
   # Enable CUPS to print documents.
