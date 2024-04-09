6955681342adfbfde5682a3054e8bf60d00a2473
Shinya Tsunematsu <s@tnmt.info>
2024-04-09 09:15:55 +0900

    stable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
@@ -25,7 +25,6 @@
         system = "x86_64-linux";
         modules = [
           ./hosts/sunflower
-          nixos-hardware.nixosModules.microsoft-surface-go
         ];
       };
     };
