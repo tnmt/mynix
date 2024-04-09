3a8727a28ee250557b9f1f3bd8a4de361b10a321
Shinya Tsunematsu <s@tnmt.info>
2024-04-09 18:03:44 +0900

    tmux config

---
+++ home/base/programs/tmux.nix
@@ -0,0 +1,26 @@
+{ pkgs
+, ...
+}: {
+  programs.tmux = {
+    enable = true;
+    keyMode = "vi";
+    shortcut = "t";
+    historyLimit = 10000;
+
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      copycat
+      yank
+      resurrect
+      pain-control
+    ];
+
+    extraConfig = ''
+      set-option -g renumber-windows on
+
+      bind e setw synchronize-panes on
+      bind E setw synchronize-panes off
+      '';
+  };
+
+}
