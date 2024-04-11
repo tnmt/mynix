de114617e0a15a9cdbba1a89dd5373a250de7414
Shinya Tsunematsu <s@tnmt.info>
2024-04-11 12:25:22 +0900

    remove old settings

--- home/base/programs/neovim.nix
+++ home/base/programs/neovim.nix
@@ -18,7 +18,6 @@
         "enable" = true;
         "startupMessage" = true;
         "hover.enable" = true;
-        "executable.command" = "~/.pyenv/versions/neovim3/bin/jedi-language-server";
       };
       "languageserver" = {
         "golang" = {
