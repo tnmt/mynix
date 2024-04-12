a674b678dd7c697e41b6d0151553d93330656d34
Shinya Tsunematsu <s@tnmt.info>
2024-04-12 15:41:20 +0900

    add comment for lanzaboote

--- hosts/sunflower/lanzaboote.nix
+++ hosts/sunflower/lanzaboote.nix
@@ -1,9 +1,16 @@
 { pkgs, lib, ... }: {
+  # This should already be here from switching to bootspec earlier.
+  # It's not required anymore, but also doesn't do any harm.
+  boot.bootspec.enable = true;
 
   environment.systemPackages = [
     pkgs.sbctl
   ];
 
+  # Lanzaboote currently replaces the systemd-boot module.
+  # This setting is usually set to true in configuration.nix
+  # generated at installation time. So we force it to false
+  # for now.
   boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.lanzaboote = {
     enable = true;
