c8667efcfb558e77f70fadcb465eb3f5d1aa2670
Shinya Tsunematsu <s@tnmt.info>
2024-04-19 21:01:19 +0900

    remove nixConfig

--- flake.nix
+++ flake.nix
@@ -110,18 +110,4 @@ in {
       };
     };
   };
-
-  nixConfig = {
-    experimental-features = [ "nix-command" "flakes" ];
-    # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
-    auto-optimise-store = true;
-
-    eval-cache = true;
-
-    substituters = [ "https://cache.nixos.org/" ];
-    extra-substituters = [ "https://nix-community.cachix.org" ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
 }
