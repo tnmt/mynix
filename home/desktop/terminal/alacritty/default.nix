55799e47fde7ec3947645684a7feffcc2106847d
Shinya Tsunematsu <s@tnmt.info>
2024-04-20 23:12:37 +0900

    indent

--- home/desktop/terminal/alacritty/default.nix
+++ home/desktop/terminal/alacritty/default.nix
@@ -4,7 +4,7 @@
 
     settings = {
       font = {
-        size = 15;
+        size = 12;
         normal = {
           family = "MesloLGS NF";
           style = "Regular";
@@ -20,32 +20,32 @@
       };
       scrolling.history = 100000;
       selection.save_to_clipboard = true;
-       colors = {
-         primary = {
-           background = "#282c34";
-           foreground = "#abb2bf";
-         };
-         normal = {
-           black   = "#1e2127";
-           red     = "#e06c75";
-           green   = "#98c379";
-           yellow  = "#d19a66";
-           blue    = "#61afef";
-           magenta = "#c678dd";
-           cyan    = "#56b6c2";
-           white   = "#abb2bf";
-         };
-         bright = {
-           black   = "#5c6370";
-           red     = "#e06c75";
-           green   = "#98c379";
-           yellow  = "#d19a66";
-           blue    = "#61afef";
-           magenta = "#c678dd";
-           cyan    = "#56b6c2";
-           white   = "#ffffff";
-         };
-       };
+      colors = {
+        primary = {
+          background = "#282c34";
+          foreground = "#abb2bf";
+        };
+        normal = {
+          black   = "#1e2127";
+          red     = "#e06c75";
+          green   = "#98c379";
+          yellow  = "#d19a66";
+          blue    = "#61afef";
+          magenta = "#c678dd";
+          cyan    = "#56b6c2";
+          white   = "#abb2bf";
+        };
+        bright = {
+          black   = "#5c6370";
+          red     = "#e06c75";
+          green   = "#98c379";
+          yellow  = "#d19a66";
+          blue    = "#61afef";
+          magenta = "#c678dd";
+          cyan    = "#56b6c2";
+          white   = "#ffffff";
+        };
+      };
     };
   };
 }
