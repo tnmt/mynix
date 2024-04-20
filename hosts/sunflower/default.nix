27144535adc22f182b78d31781950d66c02cb445
Shinya Tsunematsu <s@tnmt.info>
2024-04-20 16:11:36 +0900

    indent

--- hosts/sunflower/default.nix
+++ hosts/sunflower/default.nix
@@ -14,18 +14,20 @@
     "surface-control"
   ];
 
-  nix.buildMachines = [ {
-    hostName = "vps03";
-    system = "x86_64-linux";
-    protocol = "ssh-ng";
-    # if the builder supports building for multiple architectures,
-    # replace the previous line by, e.g.
-    # systems = ["x86_64-linux" "aarch64-linux"];
-    maxJobs = 1;
-    speedFactor = 2;
-    supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-    mandatoryFeatures = [ ];
-  }] ;
+  nix.buildMachines = [
+    {
+      hostName = "vps03";
+      system = "x86_64-linux";
+      protocol = "ssh-ng";
+      # if the builder supports building for multiple architectures,
+      # replace the previous line by, e.g.
+      # systems = ["x86_64-linux" "aarch64-linux"];
+      maxJobs = 1;
+      speedFactor = 2;
+      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      mandatoryFeatures = [ ];
+    }
+  ] ;
   nix.distributedBuilds = true;
   # optional, useful when the builder has a faster internet connection than yours
   nix.extraOptions = ''
