e37e4ac300c7781f888ecf7e1627ffd02dea298f
Shinya Tsunematsu <s@tnmt.info>
2024-04-20 14:29:14 +0900

    RTC time

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -11,6 +11,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # time sync
+  time.hardwareClockInLocalTime = true;
+
   # networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
