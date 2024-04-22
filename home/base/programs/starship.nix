1d8b15e6a7bc634373e8131b2f71fd602776da7a
Shinya Tsunematsu <s@tnmt.info>
2024-04-22 15:43:36 +0900

    starship init

--- home/base/programs/starship.nix
+++ home/base/programs/starship.nix
@@ -1,5 +1,7 @@
 { pkgs, ... } : {
   programs.starship = {
     enable = true;
+    enableZshIntegration = true;
+    settings = builtins.fromTOML (builtins.readFile ./starship/starship.toml);
   };
 }
