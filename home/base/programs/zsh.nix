b0ce8cba6c44a12430d26681f7e1395c9ae5deec
Shinya Tsunematsu <s@tnmt.info>
2024-04-22 22:01:03 +0900

    zprof

--- home/base/programs/zsh.nix
+++ home/base/programs/zsh.nix
@@ -10,9 +10,15 @@
     # zsh-syntax-highlighting を有効化する
     syntaxHighlighting.enable = true;
 
+    #initExtraFirst = ''
+    #  zmodload zsh/zprof
+    #  '';
     initExtra = ''
       . ${./zsh/zshrc}
       . ${./zsh/p10k.zsh}
+      #if (which zprof > /dev/null 2>&1) ;then
+      #  zprof
+      #fi
       '';
   };
 }
