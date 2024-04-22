3c8431a0a457743db3f0db91de87ff61a6de47dc
Shinya Tsunematsu <s@tnmt.info>
2024-04-22 23:07:52 +0900

    install latest kernel on vps03

--- hosts/vps03/default.nix
+++ hosts/vps03/default.nix
@@ -6,6 +6,8 @@
       ./hardware-configuration.nix
     ];
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/vda";
