696b76e274231976a18012f46a86bfbcb6bd9372
Shinya Tsunematsu <s@tnmt.info>
2024-04-23 11:22:56 +0900

    add vps03 again

--- hosts/sunflower/default.nix
+++ hosts/sunflower/default.nix
@@ -33,6 +33,15 @@
       supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
       mandatoryFeatures = [ ];
     }
+    {
+      hostName = "vps03";
+      system = "x86_64-linux";
+      protocol = "ssh-ng";
+      maxJobs = 1;
+      speedFactor = 2;
+      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+      mandatoryFeatures = [ ];
+    }
   ] ;
   nix.distributedBuilds = true;
   # optional, useful when the builder has a faster internet connection than yours
