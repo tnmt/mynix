9f8f1978346ffc7ced18080b63bf060eb8e60708
Shinya Tsunematsu <s@tnmt.info>
2024-04-25 00:23:54 +0900

    back to GNOME

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -43,8 +43,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  #services.xserver.displayManager.gdm.enable = true;
-  #services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
@@ -92,8 +92,8 @@
   services.xserver.displayManager.autoLogin.user = "tnmt";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  #systemd.services."getty@tty1".enable = false;
-  #systemd.services."autovt@tty1".enable = false;
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
