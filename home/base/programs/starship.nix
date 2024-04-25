ee0d34dd62429d9c78e84040ca28710f27760132
Shinya Tsunematsu <s@tnmt.info>
2024-04-25 15:37:36 +0900

    Revert "starship init"
    This reverts commit 1d8b15e6a7bc634373e8131b2f71fd602776da7a.

--- home/base/programs/starship.nix
+++ home/base/programs/starship.nix
@@ -1,7 +1,5 @@
 { pkgs, ... } : {
   programs.starship = {
     enable = true;
-    enableZshIntegration = true;
-    settings = builtins.fromTOML (builtins.readFile ./starship/starship.toml);
   };
 }
