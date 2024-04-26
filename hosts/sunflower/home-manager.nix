a08bca532488f962357a86bd258d1b2ffcb898ae
Shinya Tsunematsu <s@tnmt.info>
2024-04-26 18:52:37 +0900

    hyprland settings for sunflower

--- hosts/sunflower/home-manager.nix
+++ hosts/sunflower/home-manager.nix
@@ -3,4 +3,21 @@
     ../../home/base
     ../../home/desktop
   ];
+
+  wayland.windowManager.hyprland.settings = {
+    monitor=",highres,auto,1.6";
+    workspace = [
+      "1,monitor:eDP-1"
+      "2,monitor:eDP-1"
+      "3,monitor:eDP-1"
+      "4,monitor:eDP-1"
+      "5,monitor:eDP-1"
+      "6,monitor:eDP-1"
+      "7,monitor:eDP-1"
+      "8,monitor:eDP-1"
+      "9,monitor:eDP-1"
+      "10,monitor:eDP-1"
+    ];
+    input.kb_layout = "us";
+  };
 }
