b0d4f124ab204b9f3921ece2aaf2aaf805b4888c
Shinya Tsunematsu <s@tnmt.info>
2024-04-26 23:22:49 +0900

    fix warning

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -88,8 +88,8 @@
   };
 
   # Enable automatic login for the user.
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "tnmt";
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "tnmt";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   #systemd.services."getty@tty1".enable = false;
