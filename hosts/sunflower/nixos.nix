7a08cdd29a71839ffe933415a74ef95601dbd802
Shinya Tsunematsu <s@tnmt.info>
2024-04-26 18:14:53 +0900

    separete remote build settings and comment out somethings

--- hosts/sunflower/nixos.nix
+++ hosts/sunflower/nixos.nix
@@ -6,76 +6,17 @@
     ../../modules/nixos
     ./hardware-configuration.nix
     ./lanzaboote.nix
-    inputs.nixos-hardware.nixosModules.microsoft-surface-go
+    #inputs.nixos-hardware.nixosModules.microsoft-surface-go
     inputs.xremap-flake.nixosModules.default
   ];
 
-  microsoft-surface.surface-control.enable = true;
+  #microsoft-surface.surface-control.enable = true;
 
   users.users.tnmt.extraGroups = [
     "networkmanager"
     "wheel"
     "docker"
-    "surface-control"
-  ];
-
-  nix.buildMachines = [
-    {
-      hostName = "hydrangea";
-      system = "x86_64-linux";
-      protocol = "ssh-ng";
-      maxJobs = 1;
-      speedFactor = 2;
-      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-      mandatoryFeatures = [ ];
-    }
-    {
-      hostName = "maple";
-      system = "x86_64-linux";
-      protocol = "ssh-ng";
-      maxJobs = 1;
-      speedFactor = 2;
-      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-      mandatoryFeatures = [ ];
-    }
-    {
-      hostName = "vps03";
-      system = "x86_64-linux";
-      protocol = "ssh-ng";
-      maxJobs = 1;
-      speedFactor = 2;
-      supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-      mandatoryFeatures = [ ];
-    }
-  ] ;
-  nix.distributedBuilds = true;
-  # optional, useful when the builder has a faster internet connection than yours
-  nix.extraOptions = ''
-builders-use-substitutes = true
-    '';
-  nix.settings.trusted-public-keys = [
-  "maple:YJWnCaukPN2HZDuJtVUpyVq2szmcgkKwMEzl+fXt1DA="
-  "hydrangea:3aHkYRMqxgt9I9d8lp9ow/C0oPVTUp/0OFEObz2LRkU="
-  "vps03:C/1SqKVBylqziAX2hRuiCzQrNBB0Agfd/E0tu2rlXA8="
-  "tnmt.cachix.org-1:TWp26nryyjLq7Xzyz7Hx81W7htBNcIMcbfHw+BrxtF8="
-    ];
-  nix.settings.trusted-substituters = [
-    "ssh-ng://vps03"
-    "ssh-ng://maple"
-    "ssh-ng://hydrangea"
-  ];
-
-  nix.settings.substituters = [
-    "https://cache.nixos.org"
-    "https://tnmt.cachix.org"
-    "ssh-ng://vps03"
-    "ssh-ng://maple"
-    "ssh-ng://hydrangea"
-  ];
-
-  nix.settings.trusted-users = [
-    "root"
-    "tnmt"
+    #"surface-control"
   ];
 
   services.xserver.xkbOptions = "ctrl:nocaps, ";
