44b96547a5202beafa05f09187f5c6706c033f4e
Shinya Tsunematsu <s@tnmt.info>
2024-04-26 17:47:56 +0900

    add displays

--- hosts/maple/home-manager.nix
+++ hosts/maple/home-manager.nix
@@ -3,4 +3,21 @@
     ../../home/base
     ../../home/desktop
   ];
+
+  wayland.windowManager.hyprland.settings = {
+    workspace = [
+      "1,monitor:DP-1"
+      "2,monitor:DP-1"
+      "3,monitor:DP-1"
+      "4,monitor:DP-1"
+      "5,monitor:DP-1"
+      "6,monitor:DP-1"
+      "7,monitor:DP-1"
+      "8,monitor:DP-1"
+      "9,monitor:DP-1"
+      "10,monitor:DP-1"
+    ];
+    input.kb_layout = "us";
+  };
+
 }
