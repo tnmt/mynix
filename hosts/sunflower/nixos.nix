bde8637c060243086724e8d87a5cb5c8b709db35
Shinya Tsunematsu <s@tnmt.info>
2024-04-29 21:58:58 +0900

    core:network module

--- hosts/sunflower/nixos.nix
+++ hosts/sunflower/nixos.nix
@@ -41,6 +41,4 @@
        ];
     };
   };
-
-  networking.hostName = "sunflower";
 }
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -14,15 +14,9 @@
   # time sync
   time.hardwareClockInLocalTime = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "ja_JP.UTF-8";
