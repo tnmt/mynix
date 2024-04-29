ecc1266f783a219e8cc64a3511ffb7d20c7e2a2a
Shinya Tsunematsu <s@tnmt.info>
2024-04-29 23:23:12 +0900

    disable auto login

--- modules/programs/xserver.nix
+++ modules/programs/xserver.nix
@@ -13,8 +13,8 @@
   #services.xserver.desktopManager.gnome.enable = true;
 
   # Enable automatic login for the user.
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "tnmt";
+  #services.displayManager.autoLogin.enable = true;
+  #services.displayManager.autoLogin.user = "tnmt";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   #systemd.services."getty@tty1".enable = false;
