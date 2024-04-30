23d1c61ea56bafe9487c5561a667235ba75c711f
Shinya Tsunematsu <s@tnmt.info>
2024-04-30 15:13:13 +0900

    rich waybar settings

--- home-manager/desktop/hyprland/waybar.nix
+++ home-manager/desktop/hyprland/waybar.nix
@@ -6,4 +6,5 @@
   };
   xdg.configFile."waybar/style.css" = { source = ./waybar/style.css; };
   xdg.configFile."waybar/config" = { source = ./waybar/config; };
+  xdg.configFile."waybar/macchiato.css" = { source = ./waybar/macchiato.css; };
 }
