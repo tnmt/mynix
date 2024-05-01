d93b630dddb31220db0f791352ecde5a5d2afacc
Shinya Tsunematsu <s@tnmt.info>
2024-05-01 19:15:10 +0900

    desktop image

--- home-manager/desktop/hyprland/default.nix
+++ home-manager/desktop/hyprland/default.nix
@@ -38,4 +38,21 @@ in {
       inputs.hyprsome.packages.x86_64-linux.default # workspace manager
       hypr-helper
     ];
+
+  home.file = {
+    "abstract-purple-background.jpg" = {
+      target = ".config/hypr/wallpaper/abstract-purple-background.jpg";
+      source = pkgs.fetchurl {
+        url = "https://img.freepik.com/free-photo/abstract-purple-background-gradient-transition-generative-ai_169016-30644.jpg";
+        sha256 = "sha256-bf4REJJ/2ZsBwrq3/SLOua3dwkgd2rQaJVsZPpEL9y4=";
+      };
+    };
+    "abstract-black-background.jpg" = {
+      target = ".config/hypr/wallpaper/abstract-black-background.jpg";
+      source = pkgs.fetchurl {
+        url = "https://www.baltana.com/files/wallpapers-13/Abstract-Black-Background-HD-Desktop-Wallpapers-34039.jpg";
+        sha256 = "sha256-9SsFxvfZqwq2PhQhC5WpAMEUWv+yR59sqpZROySf/2k=";
+      };
+    };
+  };
 }
