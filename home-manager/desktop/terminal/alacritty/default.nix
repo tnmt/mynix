6c8ee7847a120b452f738a20f115ef8a0251758b
Shinya Tsunematsu <s@tnmt.info>
2024-05-01 14:33:46 +0900

    add let/in

--- home-manager/desktop/terminal/alacritty/default.nix
+++ home-manager/desktop/terminal/alacritty/default.nix
@@ -1,57 +1,62 @@
-{ ... }: {
-  programs.alacritty = {
-    enable = true;
+{
+  ...
+} :
+  let
 
-    settings = {
-      env = {
-        TERM = "xterm-256color";
+  in {
+    programs.alacritty = {
+      enable = true;
+
+      settings = {
+        env = {
+          TERM = "xterm-256color";
+        };
+        font = {
+          size = 16;
+          normal = {
+            family = "MesloLGS NF";
+            style = "Regular";
+          };
+          bold = {
+            family = "MesloLGS NF";
+            style = "Bold";
+          };
+          italic = {
+            family = "MesloLGS NF";
+            style = "Bold";
+          };
+        };
+        scrolling.history = 100000;
+        selection.save_to_clipboard = true;
+        colors = {
+          primary = {
+            background = "#282c34";
+            foreground = "#abb2bf";
+          };
+          normal = {
+            black   = "#1e2127";
+            red     = "#e06c75";
+            green   = "#98c379";
+            yellow  = "#d19a66";
+            blue    = "#61afef";
+            magenta = "#c678dd";
+            cyan    = "#56b6c2";
+            white   = "#abb2bf";
+          };
+          bright = {
+            black   = "#5c6370";
+            red     = "#e06c75";
+            green   = "#98c379";
+            yellow  = "#d19a66";
+            blue    = "#61afef";
+            magenta = "#c678dd";
+            cyan    = "#56b6c2";
+            white   = "#ffffff";
+          };
+        };
+        keyboard.bindings = [
+          { key = "N"; mods = "Control|Shift"; action = "SpawnNewInstance"; }
+        ];
       };
-      font = {
-        size = 16;
-        normal = {
-          family = "MesloLGS NF";
-          style = "Regular";
-        };
-        bold = {
-          family = "MesloLGS NF";
-          style = "Bold";
-        };
-        italic = {
-          family = "MesloLGS NF";
-          style = "Bold";
-        };
-      };
-      scrolling.history = 100000;
-      selection.save_to_clipboard = true;
-      colors = {
-        primary = {
-          background = "#282c34";
-          foreground = "#abb2bf";
-        };
-        normal = {
-          black   = "#1e2127";
-          red     = "#e06c75";
-          green   = "#98c379";
-          yellow  = "#d19a66";
-          blue    = "#61afef";
-          magenta = "#c678dd";
-          cyan    = "#56b6c2";
-          white   = "#abb2bf";
-        };
-        bright = {
-          black   = "#5c6370";
-          red     = "#e06c75";
-          green   = "#98c379";
-          yellow  = "#d19a66";
-          blue    = "#61afef";
-          magenta = "#c678dd";
-          cyan    = "#56b6c2";
-          white   = "#ffffff";
-        };
-      };
-      keyboard.bindings = [
-        { key = "N"; mods = "Control|Shift"; action = "SpawnNewInstance"; }
-      ];
     };
-  };
 }
