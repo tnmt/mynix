7aa512e44701e74b48657da5a9195b9088f4436f
Shinya Tsunematsu <s@tnmt.info>
2024-05-01 09:31:11 +0900

    retry hyprland

--- modules/programs/xserver.nix
+++ modules/programs/xserver.nix
@@ -17,13 +17,13 @@
     };
   };
 
-  services.xserver.desktopManager.gnome.enable = true;
+  #services.xserver.desktopManager.gnome.enable = true;
 
   # Enable automatic login for the user.
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "tnmt";
+  #services.displayManager.autoLogin.enable = true;
+  #services.displayManager.autoLogin.user = "tnmt";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
+  #systemd.services."getty@tty1".enable = false;
+  #systemd.services."autovt@tty1".enable = false;
 }
