ab8c588617bc166b7d14025ecd5fb26e013bd2d8
Shinya Tsunematsu <s@tnmt.info>
2024-05-02 18:38:03 +0900

    greetd

--- hosts/maple/nixos.nix
+++ hosts/maple/nixos.nix
@@ -55,7 +55,7 @@
     settings = {
       default_session = {
         command = ''
-          ${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland
+          ${pkgs.greetd.tuigreet}/bin/tuigreet --cmd Hyprland
         '';
         user = username;
       };
