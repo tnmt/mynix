beebe850e90b0c0de669ef046db7bbd1e3a6a648
Shinya Tsunematsu <s@tnmt.info>
2024-05-02 16:44:45 +0900

    swith to tokyonight colorscheme & theme

--- home-manager/desktop/terminal/alacritty/default.nix
+++ home-manager/desktop/terminal/alacritty/default.nix
@@ -33,30 +33,58 @@
         };
         scrolling.history = 100000;
         selection.save_to_clipboard = true;
+        ## tokyonight
+        #colors = {
+        #  primary = {
+        #    background = "#1a1b26";
+        #    foreground = "#a9b1d6";
+        #  };
+        #  normal = {
+        #    black   = "#32344a";
+        #    red     = "#f7768e";
+        #    green   = "#9ece6a";
+        #    yellow  = "#e0af68";
+        #    blue    = "#7aa2f7";
+        #    magenta = "#ad8ee6";
+        #    cyan    = "#449dab";
+        #    white   = "#787c99";
+        #  };
+        #  bright = {
+        #    black   = "#444b6a";
+        #    red     = "#ff7a93";
+        #    green   = "#b9f27c";
+        #    yellow  = "#ff9e64";
+        #    blue    = "#7da6ff";
+        #    magenta = "#bb9af7";
+        #    cyan    = "#0db9d7";
+        #    white   = "#acb0d0";
+        #  };
+        #};
+        ## tokyonight-storm
         colors = {
           primary = {
-            background = "#282c34";
-            foreground = "#abb2bf";
+            background = "#24283b";
+            foreground = "#a9b1d6";
           };
           normal = {
-            black   = "#1e2127";
-            red     = "#e06c75";
-            green   = "#98c379";
-            yellow  = "#d19a66";
-            blue    = "#61afef";
-            magenta = "#c678dd";
-            cyan    = "#56b6c2";
-            white   = "#abb2bf";
+            black   = "#32344a";
+            red     = "#f7768e";
+            green   = "#9ece6a";
+            yellow  = "#e0af68";
+            blue    = "#7aa2f7";
+            magenta = "#ad8ee6";
+            cyan    = "#449dab";
+            white   = "#9699a8";
           };
           bright = {
-            black   = "#5c6370";
-            red     = "#e06c75";
-            green   = "#98c379";
-            yellow  = "#d19a66";
-            blue    = "#61afef";
-            magenta = "#c678dd";
-            cyan    = "#56b6c2";
-            white   = "#ffffff";
+            black   = "#444b6a";
+            red     = "#ff7a93";
+            green   = "#b9f27c";
+            yellow  = "#ff9e64";
+            blue    = "#7da6ff";
+            magenta = "#bb9af7";
+            cyan    = "#0db9d7";
+            white   = "#acb0d0";
           };
         };
         keyboard.bindings = [
