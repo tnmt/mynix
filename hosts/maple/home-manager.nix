39533654c95ab23a1c6ae404072876a2f78d653e
Shinya Tsunematsu <s@tnmt.info>
2024-05-20 22:47:24 +0900

    replace from hyprsome to split-monitor-workspaces

--- hosts/maple/home-manager.nix
+++ hosts/maple/home-manager.nix
@@ -13,18 +13,6 @@
     env = [
       "GDK_SCALE, 2"
     ];
-    workspace = [
-      "1,monitor:DP-1"
-      "2,monitor:DP-1"
-      "3,monitor:DP-1"
-      "4,monitor:DP-1"
-      "5,monitor:DP-1"
-      "6,monitor:DP-1"
-      "7,monitor:DP-1"
-      "8,monitor:DP-1"
-      "9,monitor:DP-1"
-      "10,monitor:DP-1"
-    ];
     input.kb_layout = "us";
   };
 
--- hosts/sunflower/home-manager.nix
+++ hosts/sunflower/home-manager.nix
@@ -19,18 +19,6 @@
     input = {
       touchpad.natural_scroll = true;
     };
-    workspace = [
-      "1,monitor:eDP-1"
-      "2,monitor:eDP-1"
-      "3,monitor:eDP-1"
-      "4,monitor:eDP-1"
-      "5,monitor:eDP-1"
-      "6,monitor:eDP-1"
-      "7,monitor:eDP-1"
-      "8,monitor:eDP-1"
-      "9,monitor:eDP-1"
-      "10,monitor:eDP-1"
-    ];
     input.kb_layout = "us";
   };
 }
