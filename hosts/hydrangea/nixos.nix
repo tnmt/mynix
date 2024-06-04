9248f77250d5e2d899b0052d5a0d3386629a0c33
Shinya Tsunematsu <s@tnmt.info>
2024-06-04 11:13:12 +0900

    add obsidian-backup daily task

--- hosts/hydrangea/nixos.nix
+++ hosts/hydrangea/nixos.nix
@@ -22,10 +22,29 @@
 
   users.users."${username}" = {
     isNormalUser = true;
-    home = "/home/tnmt";
+    home = "/home/${username}";
     shell = pkgs.zsh;
     group = "users";
     extraGroups = [ "wheel" ];
+  };
+
+  systemd.timers."obsidian-backup" = {
+    description = "Run obsidian-backup.sh every 10 minutes";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "3:00";
+      Persistent = true;
+    };
+  };
+
+  systemd.services."obsidian-backup" = {
+    description = "Run obsidian-backup.sh every 10 minutes";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.bash}/bin/bash /home/${username}/backup/obsidian-backup.sh";
+      User = "${username}";
+    };
+    wantedBy = [ "multi-user.target" ];
   };
 
   nix.settings.secret-key-files = "/etc/remotebuild/cache-priv-key.pem";
