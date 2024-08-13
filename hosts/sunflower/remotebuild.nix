34bfc28dc3d862c51b5888304576d2f6260481be
Shinya Tsunematsu <s@tnmt.info>
2024-08-13 17:57:28 +0900

    replace hydrangea

--- hosts/sunflower/remotebuild.nix
+++ hosts/sunflower/remotebuild.nix
@@ -15,6 +15,20 @@
       ];
       mandatoryFeatures = [ ];
     }
+    {
+      hostName = "hydrangea";
+      system = "x86_64-linux";
+      protocol = "ssh-ng";
+      maxJobs = 1;
+      speedFactor = 2;
+      supportedFeatures = [
+        "nixos-test"
+        "benchmark"
+        "big-parallel"
+        "kvm"
+      ];
+      mandatoryFeatures = [ ];
+    }
   ];
 
   nix.distributedBuilds = true;
@@ -24,19 +38,19 @@
   '';
   nix.settings.trusted-public-keys = [
     "maple:YJWnCaukPN2HZDuJtVUpyVq2szmcgkKwMEzl+fXt1DA="
-    "hydrangea:3aHkYRMqxgt9I9d8lp9ow/C0oPVTUp/0OFEObz2LRkU="
+    "hydrangea:iq3NX80BoEVA8m/+RF2xxirVdBVkvsdE75+elmlwQAI="
     "vps03:C/1SqKVBylqziAX2hRuiCzQrNBB0Agfd/E0tu2rlXA8="
   ];
   nix.settings.trusted-substituters = [
-    "ssh-ng://maple"
+    "ssh-ng://hydrangea"
+    #"ssh-ng://maple"
     #"ssh-ng://vps03"
-    #"ssh-ng://hydrangea"
   ];
 
   nix.settings.substituters = [
     "https://cache.nixos.org"
-    "ssh-ng://maple"
+    "ssh-ng://hydrangea"
+    #"ssh-ng://maple"
     #"ssh-ng://vps03"
-    #"ssh-ng://hydrangea"
   ];
 }
