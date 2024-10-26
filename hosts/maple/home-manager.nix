580892211cbc8eca7f59ca7c21ee028c09270f4b
Shinya Tsunematsu <tnmt@maple.tnmt.info>
2024-10-26 18:33:02 +0900

    maple turns into Ubuntu from NixOS

--- hosts/maple/home-manager.nix
+++ hosts/maple/home-manager.nix
@@ -2,14 +2,5 @@
 {
   imports = [
     ../../home-manager/base
-    ../../home-manager/desktop
-    ../../home-manager/desktop/terminal
-    ../../home-manager/desktop/hyprland
   ];
-
-  wayland.windowManager.hyprland.settings = {
-    monitor = [ "DP-1, 3840x2160@60, 0x0, 2" ];
-    env = [ "GDK_SCALE, 2" ];
-    input.kb_layout = "us";
-  };
 }
