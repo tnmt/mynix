5d62a418837de114cd9e6e0c1bf0f0aba9bb1669
Shinya Tsunematsu <s@tnmt.info>
2025-07-24 23:35:31 +0900

    add oneaws flake for work environments

--- flake.nix
+++ flake.nix
@@ -28,6 +28,7 @@
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
 
     ccusage.url = "github:tnmt/ccusage-flake";
+    oneaws.url = "github:tnmt/oneaws-flake";
 
     # Hyprland
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
--- home-manager/work/default.nix
+++ home-manager/work/default.nix
@@ -1,6 +1,9 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 {
   home.packages = with pkgs; [
     openstackclient
+
+    # oneaws
+    inputs.oneaws.packages.${pkgs.system}.default
   ];
 }
