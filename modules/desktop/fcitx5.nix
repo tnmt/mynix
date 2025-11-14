330403a3c10da2a9cef3431e6d2531cfb51d7cf4
常松 伸哉 <s@tnmt.info>
2025-11-14 15:03:11 +0900

    update fcitx5

--- modules/desktop/fcitx5.nix
+++ modules/desktop/fcitx5.nix
@@ -1,10 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   i18n.inputMethod = {
-    enabled = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-mozc
-      fcitx5-gtk
-    ];
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-mozc
+        fcitx5-gtk
+        fcitx5-qt
+        fcitx5-configtool
+      ];
+    };
   };
 }
