d8af12c2607b926b58e9b60c57ba94d5256f7ea1
Shinya Tsunematsu <s@tnmt.info>
2026-03-31 22:51:40 +0900

    Use start-hyprland in greetd

--- hosts/dahlia/nixos.nix
+++ hosts/dahlia/nixos.nix
@@ -23,7 +23,7 @@
   services.greetd = {
     enable = true;
     settings.default_session = {
-      command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
+      command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd start-hyprland";
       user = "greeter";
     };
   };
