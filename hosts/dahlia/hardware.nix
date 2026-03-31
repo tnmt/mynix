13b95b3d7615a9d93799540dd1cf992eff71425e
Shinya Tsunematsu <s@tnmt.info>
2026-03-31 18:20:49 +0900

    fix dahlia settings

--- hosts/dahlia/hardware.nix
+++ hosts/dahlia/hardware.nix
@@ -21,18 +21,18 @@
   #       Run `nixos-generate-config` on the installed system
   #       and update these UUIDs accordingly.
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/CHANGEME-ROOT-UUID";
+    device = "/dev/disk/by-uuid/9a75e7f7-cd89-4e04-a13e-f51f2289909f";
     fsType = "ext4";
   };
 
   fileSystems."/efi" = {
-    device = "/dev/disk/by-uuid/CHANGEME-EFI-UUID";
+    device = "/dev/disk/by-uuid/4AEC-514E";
     fsType = "vfat";
     options = [ "fmask=0077" "dmask=0077" ];
   };
 
   swapDevices = [
-    { device = "/dev/disk/by-uuid/CHANGEME-SWAP-UUID"; }
+    { device = "/dev/disk/by-uuid/0568ef41-b667-4dcb-893a-a5f7d7b0899f"; }
   ];
 
   # GPU - AMD Renoir (Radeon Vega Series)
