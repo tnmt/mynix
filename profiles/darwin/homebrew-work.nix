2b129e05d78f6507b62a815e9a6ce6535ce814a2
Shinya Tsunematsu <s@tnmt.info>
2026-04-17 00:15:51 +0900

    Extract Darwin homebrew profiles

---
+++ profiles/darwin/homebrew-work.nix
@@ -0,0 +1,29 @@
+{ ... }:
+{
+  homebrew = {
+    taps = [
+      "goreleaser/tap"
+      "hashicorp/tap"
+      "pyama86/kagiana"
+      "rtk-ai/tap"
+    ];
+    brews = [
+      "azure-cli"
+      "colima"
+      "consul-template"
+      "docker"
+      "envchain"
+      "hashicorp/tap/vault"
+      "pyama86/kagiana/kagiana"
+      "rtk-ai/tap/rtk"
+    ];
+    casks = [
+      "cmux"
+      "goreleaser"
+      "kiro"
+      "meetingbar"
+      "notion"
+      "visual-studio-code"
+    ];
+  };
+}
