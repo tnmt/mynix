d6d7c3887769d57572a61355cd06f20c06f49475
Shinya Tsunematsu <s@tnmt.info>
2026-04-20 03:50:48 +0900

    darwin: move home-manager integration to module

---
+++ home-manager/defaults.nix
@@ -0,0 +1,32 @@
+{
+  homeDirectory,
+  sopsFile,
+  username,
+}:
+{
+  lib,
+  pkgs,
+  ...
+}:
+{
+  home = {
+    inherit username homeDirectory;
+    stateVersion = "25.05";
+  };
+
+  nix.package = lib.mkDefault pkgs.nix;
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  sops = {
+    defaultSopsFile = sopsFile;
+    age.keyFile = "${homeDirectory}/.config/sops/age/keys.txt";
+    secrets = {
+      git_email = { };
+      git_name = { };
+      atuin_sync_address = {
+        sopsFile = ../secrets/common.yaml;
+      };
+    };
+  };
+}
